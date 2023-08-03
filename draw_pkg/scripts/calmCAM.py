5926770c467ee65f949fea8be195b275b64740be
Ajay Gunalan <ajaygunalan1995@gmail.com>
2023-08-03 16:11:49 +0200

    calmCAM will srream CALM camera

--- draw_pkg/scripts/calmCAM.py
+++ draw_pkg/scripts/calmCAM.py
@@ -1,19 +1,14 @@
 import cv2
   
-  
-# define a video capture object
 vid = cv2.VideoCapture(2)
 a  =  vid.getBackendName()
   
 while(True):
-      
-    # Capture the video frame
-    # by frame
+
     ret, frame = vid.read()
 
     cv2.imshow('frame', frame)
       
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
   
