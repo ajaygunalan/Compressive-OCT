e53bd8f95f48c85fc7b15885910938fea559f22e
Ajay Gunalan <ajaygunalan1995@gmail.com>
2023-09-15 18:32:54 +0200

    added the manual for Thorlabs spectar rada API

--- OCT_ROS_server.m
+++ OCT_ROS_server.m
@@ -11,18 +11,29 @@ service = rossvcserver('/estimate_depth', 'oct_msgs/Depth', @serviceCallback);
 % Setup cleanup function to ensure rosshutdown is called when script ends
 cleanupObj = onCleanup(@()rosshutdown);
 
-disp('Service Server running. CTRL+C to exit.');
+disp('OCT Server (ROS Service) running. CTRL+C to exit.');
 
 % Callback function
 function resp = serviceCallback(~,~,resp)
+    disp('OCT Server: CALM client is requestsing depth.');
+
+    userResponse = input('\nOCT Server: Type "ok" to perform imaging, anything else to quit.', 's');
+    
+    if ~strcmp(userResponse, 'ok')
+        fprintf('\nOCT Server: Execution cancelled by the user. Exiting...\n');
+        rosshutdown;
+        return;
+    end
+  
     % Perform imaging
     % system('".\bin\x64\Debug\OCTImageCapture.exe"'); % Call .exe file to capture image
+    % disp('OCT Server: Imaging done.');
 
     % User checks the image manually
-    userResponse = input('\nPlease check the images. Type "ok" to continue if the images are OK, anything else to quit.', 's');
+    userResponse = input('\nOCT Server: Please check the images. Type "ok" to continue if the images are OK, anything else to quit.', 's');
 
     if ~strcmp(userResponse, 'ok')
-        fprintf('\nExecution cancelled by the user. Exiting...\n');
+        fprintf('\nOCT Server: Execution cancelled by the user. Exiting...\n');
         rosshutdown;
         return;
     end
@@ -32,17 +43,17 @@ function resp = serviceCallback(~,~,resp)
     estimatedDepth = 5.0; % placeholder, replace with actual depth calculation
 
     % User checks the estimated depth manually
-    userResponse = input(['\nEstimated depth: ', num2str(estimatedDepth), '. Type "ok" to continue if the depth is OK, anything else to quit.'], 's');
+    userResponse = input(['\nOCT Server: Estimated depth: ', num2str(estimatedDepth), '. Type "ok" to continue if the depth is OK, anything else to quit.'], 's');
 
     if ~strcmp(userResponse, 'ok')
-        fprintf('\nExecution cancelled by the user. Exiting...\n');
+        fprintf('\nOCT Server: Execution cancelled by the user. Exiting...\n');
         rosshutdown;
         return;
     end
 
     resp.Depth = rosmessage('std_msgs/Float64');
     resp.Depth.Data = estimatedDepth;
-    disp("Depth estimated and sent: " + num2str(estimatedDepth));
+    disp("OCT Server: Depth estimated and sent: " + num2str(estimatedDepth));
 end
 
 
