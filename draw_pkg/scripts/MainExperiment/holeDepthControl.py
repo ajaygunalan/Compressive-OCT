61fdee5aa58747d0a31baa5065ec428138bad570
Ajay Gunalan <ajaygunalan1995@gmail.com>
2023-09-22 18:11:17 +0200

    1st loop works

--- draw_pkg/scripts/MainExperiment/holeDepthControl.py
+++ draw_pkg/scripts/MainExperiment/holeDepthControl.py
@@ -4,14 +4,14 @@ import rospy
 from oct_msgs.srv import Depth
 
 # Parameters
-port = "/dev/ttyACM1"
+port = "/dev/ttyACM0"
 baud_rate = 115200
-desired_depth = 10.0
+desired_depth = 2.0
 Kp = 0.5
 Kd = 0.2
 prev_error = 0.0
 max_time_on = 2.0  # Hard maximum limit for time_on
-initial_ablation_time = 0.5  # in seconds
+initial_ablation_time = 1.0 # in seconds
 
 # Function to send continuous command
 def send_continuous_command(ser, command, duration):
@@ -24,6 +24,15 @@ def send_continuous_command(ser, command, duration):
 rospy.init_node('depth_client')
 rospy.wait_for_service('/estimate_depth')
 estimate_depth = rospy.ServiceProxy('/estimate_depth', Depth)
+
+print("CALM Client established connection with OCT Server.")
+print(f"Initial ablation time: {initial_ablation_time} seconds")
+print(f"Desired Depth: {desired_depth} mm")
+proceed_experiment = input('Do you want to proceed with the experiment? (yes/no): ')
+
+if proceed_experiment.lower() != 'yes':
+    print("Exiting as user did not grant permission for the experiment.")
+    exit()
 
 try:
     with serial.Serial(port, baud_rate) as ser:
@@ -41,12 +50,13 @@ try:
         while not rospy.is_shutdown():
 
             # Check depth
+            print("Requested depth from OCT Server")
             resp = estimate_depth()
             current_depth = resp.depth
             print("Current depth: ", current_depth)
 
-            if current_depth < desired_depth:
-                error = desired_depth - current_depth
+            if current_depth.data < desired_depth:
+                error = desired_depth - current_depth.data
                 print("Depth difference: ", error)
                 derivative = error - prev_error
                 time_on = min(Kp * error + Kd * derivative, max_time_on)
