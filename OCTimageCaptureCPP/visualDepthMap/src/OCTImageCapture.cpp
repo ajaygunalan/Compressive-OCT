8f3a6e60e32e21cc228ed3a3eb34f368cf594148
Ajay Gunalan <ajaygunalan1995@gmail.com>
2023-10-09 15:18:16 +0200

    added horiztonal line for CS

--- OCTimageCaptureCPP/visualDepthMap/src/OCTImageCapture.cpp
+++ OCTimageCaptureCPP/visualDepthMap/src/OCTImageCapture.cpp
@@ -15,15 +15,18 @@ using namespace std;
 
 
 
-void ExportAScanImage(string n, double NumOfAScan, double PosX, double PosY) {
+void ExportAScanImage(string n, double NumOfAScan, double PosX, double PosY, const string& filepath) {
 	char message[1024];
 
 	OCTDeviceHandle Dev = initDevice();
-	ProbeHandle Probe = initProbe(Dev, "Probe_Standard_OCTG_LSM03.ini");
+	ProbeHandle Probe = initProbe(Dev, "Probe_Standard_OCTG_LSM04.ini");
 	ProcessingHandle Proc = createProcessingForDevice(Dev);
 
 	RawDataHandle Raw = createRawData();
 	DataHandle AScanDH = createData();
+
+	ColoredDataHandle VideoImg = createColoredData();
+	ColoredDataHandle VideoImgCopy = createColoredData();
 
 	if (getError(message, 1024)) {
 		cout << "ERROR: " << message << endl;
@@ -47,12 +50,51 @@ void ExportAScanImage(string n, double NumOfAScan, double PosX, double PosY) {
 	executeProcessing(Proc, Raw);
 	stopMeasurement(Dev);
 
-	string filepath = "C:\\Ajay_OCT\\OCTAssistedSurgicalLaserbot\\data\\oct";
-	string exten = ".csv";
-	string fullpath = filepath + n + exten;
-	const char* cstr = fullpath.c_str();
+	// Convert PosX and PosY to integer to truncate any decimal portions
+	int posXInt = static_cast<int>(PosX);
+	int posYInt = static_cast<int>(PosY);
 
-	exportData(AScanDH, DataExport_CSV, cstr);
+	// Construct the filename with the naming convention
+	std::string namePrefix = "oct";
+	if (posXInt < 0 || posYInt < 0) {
+		namePrefix += "_";
+	}
+	std::string fileName = namePrefix + std::to_string(std::abs(posXInt)) + std::to_string(std::abs(posYInt));
+
+	// Save .csv file
+	string csvPath = filepath + fileName + ".csv";
+	const char* cstrCSV = csvPath.c_str();
+	exportData(AScanDH, DataExport_CSV, cstrCSV);
+
+
+	visualizeScanPatternOnDevice(Dev, Probe, Pattern, TRUE);
+
+	getCameraImage(Dev, VideoImg);
+	visualizeScanPatternOnImage(Probe, Pattern, VideoImg);
+
+	copyColoredData(VideoImg, VideoImgCopy);
+	unsigned long* data = getColoredDataPtr(VideoImgCopy);
+
+	int width = 648, height = 484;
+	cv::Mat videoImagecv = cv::Mat(height, width, CV_8UC3);
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			unsigned long pixelValue = data[i * width + j];
+			cv::Vec3b& pixel = videoImagecv.at<cv::Vec3b>(i, j);
+			pixel[0] = (pixelValue >> 16) & 0xFF; // Blue channel
+			pixel[1] = (pixelValue >> 8) & 0xFF;  // Green channel
+			pixel[2] = pixelValue & 0xFF;         // Red channel
+		}
+	}
+	data = nullptr;
+
+
+	string jpgPath = filepath + "scan_pattern" + fileName + ".jpg";
+	const char* cstrJPG = jpgPath.c_str();
+	cv::imwrite(cstrJPG, videoImagecv);
+
+
+	cv::destroyAllWindows();
 
 	clearScanPattern(Pattern);
 	clearData(AScanDH);
@@ -62,11 +104,11 @@ void ExportAScanImage(string n, double NumOfAScan, double PosX, double PosY) {
 	closeDevice(Dev);
 }
 
-void ExportBScanImage(string n, double BScanRangeMM, double ShiftX, double ShiftY, double Angle_rad) {
+void ExportBScanImage(string n, double BScanRangeMM, double ShiftX, double ShiftY, double Angle_rad, const string& filepath) {
 	char message[1024];
 
 	OCTDeviceHandle Dev = initDevice();
-	ProbeHandle Probe = initProbe(Dev, "Probe_Standard_OCTG_LSM03.ini");
+	ProbeHandle Probe = initProbe(Dev, "Probe_Standard_OCTG_LSM04.ini");
 	ProcessingHandle Proc = createProcessingForDevice(Dev);
 
 	RawDataHandle Raw = createRawData();
@@ -104,11 +146,8 @@ void ExportBScanImage(string n, double BScanRangeMM, double ShiftX, double Shift
 	// Exports the processed data to an image with the specified slice normal direction since this will result in 2D-images.
 	// To get the B-scan in one image with depth and scan field as axes for a single B-scan #Direction_3 is chosen.
 
-	string filepath = "C:\\Ajay_OCT\\OCTAssistedSurgicalLaserbot\\data\\oct";
-	string exten = ".jpg";
-	string fullpath = filepath + n + exten;
+	string fullpath = filepath + "oct" + n + ".jpg";
 	const char* cstr = fullpath.c_str();
-
 	exportDataAsImage(BScanDH, Coloring, ColoredDataExport_JPG, Direction_3, cstr, ExportOption_DrawScaleBar | ExportOption_DrawMarkers | ExportOption_UsePhysicalAspectRatio);
 	//cv::Mat OCTimage = cv::imread("C:/Ajay_OCT/visualDepthMap/data/oct.jpg", cv::IMREAD_COLOR);
 	//std::thread t(ProcessAndSaveImage, Dev, Probe, Pattern, n);
@@ -137,9 +176,7 @@ void ExportBScanImage(string n, double BScanRangeMM, double ShiftX, double Shift
 	}
 	data = nullptr;
 
-	filepath = "C:\\Ajay_OCT\\OCTAssistedSurgicalLaserbot\\data\\scanPattern";
-	exten = ".jpg";
-	fullpath = filepath + n + exten;
+	fullpath = filepath + "scan_pattern" + n + ".jpg";
 	cstr = fullpath.c_str();
 	cv::imwrite(cstr, videoImagecv);
 
@@ -154,31 +191,52 @@ void ExportBScanImage(string n, double BScanRangeMM, double ShiftX, double Shift
 }
 
 int main(int argc, char* argv[]) {
-	/**
-	if (argc < 5) {
-		cout << "Insufficient arguments. Usage: ./YourProgram <BScanRangeMM> <ShiftX> <ShiftY> <Angle_deg>" << endl;
-		return 1;
-	}
-
-	double BScanRangeMM = atof(argv[1]);
-	double ShiftX = atof(argv[2]);
-	double ShiftY = atof(argv[3]);
-	double Angle_deg = atof(argv[4]);
-	double Angle_rad = Angle_deg * (PI / 180);
-
-	ExportBScanImage("1", BScanRangeMM, ShiftX, ShiftY, Angle_rad);
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-
-
-	double NumOfAScan = atof(argv[1]);
-	double PosX = atof(argv[2]);
-	double PosY = atof(argv[3]);
-	**/
-
+	string filepath = "C:\\Ajay_OCT\\OCTAssistedSurgicalLaserbot\\data\\cs\\ablated_plaster\\";
 	double NumOfAScan = 5;
-	double PosX = 0.0;
-	double PosY = 0.0;
 
-	ExportAScanImage("1", NumOfAScan, PosX, PosY);
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::vector<std::pair<double, double>> horizontalLine = {
+		{7.0, 0.0},
+		{6.0, 0.0},
+		{5.0, 0.0},
+		{4.0, 0.0},
+		{3.0, 0.0},
+		{2.0, 0.0},
+		{1.0, 0.0},
+		{0.0, 0.0},
+		{-1.0, 0.0},
+		{-2.0, 0.0},
+		{-3.0, 0.0},
+		{-4.0, 0.0},
+		{-5.0, 0.0},
+		{-6.0, 0.0},
+		{-7.0, 0.0}
+	};
+
+	std::vector<std::pair<double, double>> verticalline = {
+	{-1.0, 7.0},
+	{-1.0, 6.0},
+	{-1.0, 5.0},
+	{-1.0, 4.0},
+	{-1.0, 3.0},
+	{-1.0, 2.0},
+	{-1.0, 1.0},
+	{-1.0, 0.0},
+	{-1.0, -1.0},
+	{-1.0, -2.0},
+	{-1.0, -3.0},
+	{-1.0, -4.0},
+	{-1.0, -5.0},
+	{-1.0, -6.0},
+	{-1.0, -7.0}
+	};
+
+	std::vector<std::pair<double, double>> scanLocations = horizontalLine;
+
+	for (const auto& location : scanLocations) {
+		double PosX = location.first;
+		double PosY = location.second;
+		string n = std::to_string(static_cast<int>(PosX));
+		ExportAScanImage(n, NumOfAScan, PosX, PosY, filepath);
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
 }
