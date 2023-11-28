0e35a99b4437d200145495282691d39683c8d1a1
Ajay Gunalan <ajaygunalan1995@gmail.com>
2023-11-28 13:20:31 +0100

    400 data points

--- OCTimageCaptureCPP/visualDepthMap/src/OCTImageCapture.cpp
+++ OCTimageCaptureCPP/visualDepthMap/src/OCTImageCapture.cpp
@@ -42,22 +42,23 @@ struct ScanResult {
     double actualTime;
     double expectedTime;
     int numOfLostBScan;
-    int BscanCompressionRatio;
-    int CscanCompressionRatio;
 };
 
 
-void getSurfaceFrom3DScan(const std::string& folderLocation, std::string fileName,  int NumAScansPerBScanReference, double LengthOfBScan, int NumBScansPerVolumeReference, double WidthOfVolume) {
+ScanResult getSurfaceFrom3DScan(int AScansPerBScan, double LengthOfBScan, int BScansPerVolume, double WidthOfVolume) {
     char message[1024];
     OCTDeviceHandle Dev = initDevice();
     ProbeHandle Probe = initProbe(Dev, "Probe_Standard_OCTG_LSM04.ini");
     ProcessingHandle Proc = createProcessingForDevice(Dev);
 
-
+    RawDataHandle RawVolume = createRawData();
+    DataHandle Volume = createData();
+    DataHandle Surface = createData();
 
     if (getError(message, 1024)) {
         std::cout << "ERROR: " << message << std::endl;
         _getch();
+        return ScanResult{ nullptr, -1.0, -1.0 };  // Indicate error with negative times
     }
 
     setDevicePreset(Dev, CATEGORY_SPEED_SENSITIVITY, Probe, Proc, PRESET_HIGH_SPEED_146kHz);
@@ -65,88 +66,65 @@ void getSurfaceFrom3DScan(const std::string& folderLocation, std::string fileNam
     setProbeParameterInt(Probe, Probe_Oversampling, AScanAveraging); // this results in a repetition of each scan point in the B-scan
     setProcessingParameterInt(Proc, Processing_AScanAveraging, AScanAveraging);
 
+    ScanPatternHandle Pattern = createVolumePattern(Probe, LengthOfBScan, AScansPerBScan, WidthOfVolume, BScansPerVolume, ScanPattern_ApoOneForAll, ScanPattern_AcqOrderAll);
     
-    for (double BscanCompressionRatio = 0.25; BscanCompressionRatio >= 0.05; BscanCompressionRatio -= 0.05) {
-        for (double CscanCompressionRatio = 1.0; CscanCompressionRatio >= 0.05; CscanCompressionRatio -= 0.05) {
-            int numAScansPerBScan = static_cast<int>(NumAScansPerBScanReference * BscanCompressionRatio);
-            int numBScansPerVolume = static_cast<int>(NumBScansPerVolumeReference * CscanCompressionRatio);
 
-            RawDataHandle RawVolume = createRawData();
-            DataHandle Volume = createData();
-            DataHandle Surface = createData();
-            ScanPatternHandle Pattern = createVolumePattern(Probe, LengthOfBScan, numAScansPerBScan, WidthOfVolume, numBScansPerVolume, ScanPattern_ApoOneForAll, ScanPattern_AcqOrderAll);
+    // the apodization spectra are acquired now
+    //measureApodizationSpectra(Dev, Probe, Proc);
 
-
-            // the apodization spectra are acquired now
-            //measureApodizationSpectra(Dev, Probe, Proc);
-
-            auto start = std::chrono::high_resolution_clock::now();
-            startMeasurement(Dev, Pattern, Acquisition_AsyncContinuous);
-            getRawData(Dev, RawVolume);
-            setProcessedDataOutput(Proc, Volume);
-            executeProcessing(Proc, RawVolume);
-            stopMeasurement(Dev);
-            auto stop = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
+    startMeasurement(Dev, Pattern, Acquisition_AsyncContinuous);
+    getRawData(Dev, RawVolume);
+    setProcessedDataOutput(Proc, Volume);
+    executeProcessing(Proc, RawVolume);
+    stopMeasurement(Dev);
+    auto stop = std::chrono::high_resolution_clock::now();
 
 
-            int numOfLostBScan = getRawDataPropertyInt(RawVolume, RawData_LostFrames);
-            std::chrono::duration<double> actualTimeDuration = stop - start;
-            double actualTime = actualTimeDuration.count();
-            double expectedTime = expectedAcquisitionTime_s(Pattern, Dev);
+    int numOfLostBScan = getRawDataPropertyInt(RawVolume, RawData_LostFrames);
 
-            determineSurface(Volume, Surface);
+    std::chrono::duration<double> actualTimeDuration = stop - start;
+    double actualTime = actualTimeDuration.count();
+    double expectedTime = expectedAcquisitionTime_s(Pattern, Dev);
 
-            ScanResult result;
-            result.surface = Surface;
-            result.actualTime = actualTimeDuration.count();
-            result.expectedTime = expectedTime;
-            result.numOfLostBScan = numOfLostBScan;
-            result.BscanCompressionRatio = BscanCompressionRatio;
-            result.CscanCompressionRatio = CscanCompressionRatio;
+    determineSurface(Volume, Surface);
 
-            processScanData(result, folderLocation, fileName);  // Process the data from this iteration
-
-            // Clean up
-            clearScanPattern(Pattern);
-            clearData(Volume);
-            clearRawData(RawVolume);
-
-        }
-    }
+    // Clean up
+    clearScanPattern(Pattern);
+    clearData(Volume);
+    clearRawData(RawVolume);
     clearProcessing(Proc);
     closeProbe(Probe);
     closeDevice(Dev);
+
+    return ScanResult{ Surface, actualTime, expectedTime, numOfLostBScan};
 }
 
-void processScanData(const ScanResult& result, const std::string& folderLocation, const std::string& baseFileName) {
-    // Exporting surface data
-    std::string surfaceFileName = folderLocation + baseFileName + "_surface.csv";
-    if (result.surface) {
-        exportData(result.surface, DataExport_CSV, surfaceFileName.c_str());
-    }
-    else {
-        std::cerr << "No surface data to export.\n";
-    }
 
-    // Writing metadata to a file
-    std::string metaFileName = folderLocation + baseFileName + "_meta.csv";
-    std::ofstream metaFile(metaFileName);
+void performScanAndExport(const std::string& folderLocation, std::string fileName,
+    int numAScans, int numBScans, double LengthOfBScan, double WidthOfVolume,
+    double BscanCompressionRatio, double CscanCompressionRatio) {
+    ScanResult result = getSurfaceFrom3DScan(numAScans, LengthOfBScan, numBScans, WidthOfVolume);
+    if (result.surface) {
+        exportData(result.surface, DataExport_CSV, (folderLocation + fileName + ".csv").c_str());
+    }
+    std::ofstream metaFile(folderLocation + fileName + "_meta.csv");
     if (metaFile.is_open()) {
-        metaFile << "Number of B-Scans: " << result.numBScans << "\n";
-        metaFile << "Number of A-Scans per B-Scan: " << result.numAScans << "\n";
-        metaFile << "B-Scan Compression Ratio: " << result.BscanCompressionRatio << "\n";
-        metaFile << "C-Scan Compression Ratio: " << result.CscanCompressionRatio << "\n";
-        metaFile << "Actual Time: " << result.actualTime << "\n";
-        metaFile << "Expected Time: " << result.expectedTime << "\n";
-        metaFile << "Number of Lost B-Scans: " << result.numOfLostBScan << "\n";
+        metaFile << numBScans << "\n";
+        metaFile << numAScans << "\n";
+        metaFile << BscanCompressionRatio << "\n";
+        metaFile << CscanCompressionRatio << "\n";
+        metaFile << result.actualTime << "\n";
+        metaFile << LengthOfBScan << "\n";
+        metaFile << WidthOfVolume << "\n";
+        metaFile << result.numOfLostBScan << "\n";
+        metaFile << result.expectedTime;
         metaFile.close();
     }
     else {
-        std::cerr << "Unable to open file for writing metadata: " << metaFileName << "\n";
+        std::cerr << "Unable to open file for writing metadata\n";
     }
 }
-
-
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -167,7 +145,17 @@ int main(int argc, char* argv[]) {
     int NumAScansPerBScanReference = 256;
     int NumBScansPerVolumeReference = 100;
 
-    getSurfaceFrom3DScan(folderLocation, fileName, numAScansPerBScan, numBScansPerVolume, LengthOfBScan, WidthOfVolume, BscanCompressionRatio, CscanCompressionRatio);
-     
+    for (double BscanCompressionRatio = 1.0; BscanCompressionRatio >= 0.05; BscanCompressionRatio -= 0.05) {
+        for (double CscanCompressionRatio = 1.0; CscanCompressionRatio >= 0.05; CscanCompressionRatio -= 0.05) {
+            int numAScansPerBScan = static_cast<int>(NumAScansPerBScanReference * BscanCompressionRatio);
+            int numBScansPerVolume = static_cast<int>(NumBScansPerVolumeReference * CscanCompressionRatio);
+
+            std::string fileName = "CompressionRatio_Bscan" + to_string(BscanCompressionRatio) +
+                                   "_AScan" + to_string(CscanCompressionRatio);
+
+            performScanAndExport(folderLocation, fileName, numAScansPerBScan, numBScansPerVolume, LengthOfBScan, WidthOfVolume, BscanCompressionRatio, CscanCompressionRatio);
+        }
+    }
+
     return 0;
 }
