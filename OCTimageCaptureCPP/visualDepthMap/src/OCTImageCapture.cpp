bfe50b22274180cbe100a158513ce9a914c1f1c8
Ajay Gunalan <ajaygunalan1995@gmail.com>
2023-12-11 17:25:51 +0100

    dataAnalysios script ready

--- OCTimageCaptureCPP/visualDepthMap/src/OCTImageCapture.cpp
+++ OCTimageCaptureCPP/visualDepthMap/src/OCTImageCapture.cpp
@@ -103,13 +103,12 @@ void getSurfaceFrom3DScan(const std::string& folderLocation, int NumAScansPerBSc
 
     int count = 1;
     std::vector<std::tuple<double, double>> compressionPairs = {
-    {1.0, 1.0}, {0.5, 0.5}, 
-                {1.0, 0.9}, {1.0, 0.8}, {1.0, 0.7}, {1.0, 0.6}, {1.0, 0.5},
-    {0.9, 1.0}, {0.9, 0.9}, {0.9, 0.8}, {0.9, 0.7}, {0.9, 0.6}, {0.9, 0.5},
-    {0.8, 1.0}, {0.8, 0.9}, {0.8, 0.8}, {0.8, 0.7}, {0.8, 0.6}, {0.8, 0.5},
-    {0.7, 1.0}, {0.7, 0.9}, {0.7, 0.8}, {0.7, 0.7}, {0.7, 0.6}, {0.7, 0.5},
-    {0.6, 1.0}, {0.6, 0.9}, {0.6, 0.8}, {0.6, 0.7}, {0.6, 0.6}, {0.6, 0.5},
-    {0.5, 1.0}, {0.5, 0.9}, {0.5, 0.8}, {0.5, 0.7}, {0.5, 0.6}};
+    {1.0, 1.0}, {0.5, 0.5},
+                {1.0, 0.9}, {1.0, 0.8}, {1.0, 0.5},
+    {0.9, 1.0}, {0.9, 0.9}, {0.9, 0.8}, {0.9, 0.5},
+    {0.8, 1.0}, {0.8, 0.9}, {0.8, 0.8}, {0.8, 0.5},
+    {0.5, 1.0}, {0.5, 0.9}, {0.5, 0.8}, 
+    };
 
     for (const auto& [BscanCompressionRatio, CscanCompressionRatio] : compressionPairs) {
         int numAScansPerBScan = static_cast<int>(NumAScansPerBScanReference * BscanCompressionRatio);
---
+++ dataAnalysis.m
@@ -0,0 +1,61 @@
+% Set base folder
+baseFolder = 'C:\Ajay_OCT\OCT-Guided-AutoCALM\data\getDepthFromSparse3Doct\';
+
+% Number of trials
+numTrials = 25;
+
+% Initialize arrays to store results
+allNetReductionFactors = zeros(10, 10, numTrials);
+allReconstructionTimes = zeros(10, 10, numTrials);
+allScanTimes = zeros(10, 10, numTrials);
+allReconstructionErrors = zeros(10, 10, numTrials);
+
+% Iterate through each trial
+for trialNum = 1:numTrials
+    % Construct folder location for each trial
+    folderLocation = fullfile(baseFolder, num2str(trialNum));
+
+    % Construct file name for the results file
+    saveFileName = fullfile(folderLocation, 'Results.mat');
+
+    % Load the data if the file exists
+    if exist(saveFileName, 'file')
+        load(saveFileName, 'NetReductionFactor', 'ReconstructionTime', 'ScanTime', 'ReconstructionError');
+        allNetReductionFactors(:, :, trialNum) = NetReductionFactor;
+        allReconstructionTimes(:, :, trialNum) = ReconstructionTime;
+        allScanTimes(:, :, trialNum) = ScanTime;
+        allReconstructionErrors(:, :, trialNum) = ReconstructionError;
+    else
+        fprintf('Warning: Results.mat not found for trial number %d\n', trialNum);
+    end
+end
+
+% Calculate the mean and standard deviation for each metric
+meanNetReductionFactors = mean(allNetReductionFactors, 3);
+stdDevNetReductionFactors = std(allNetReductionFactors, 0, 3);
+
+meanReconstructionTimes = mean(allReconstructionTimes, 3);
+stdDevReconstructionTimes = std(allReconstructionTimes, 0, 3);
+
+meanScanTimes = mean(allScanTimes, 3);
+stdDevScanTimes = std(allScanTimes, 0, 3);
+
+meanReconstructionErrors = mean(allReconstructionErrors, 3);
+stdDevReconstructionErrors = std(allReconstructionErrors, 0, 3);
+
+% Display the calculated mean and standard deviation for each metric
+disp('Mean and Standard Deviation of Net Reduction Factors:');
+disp(meanNetReductionFactors);
+disp(stdDevNetReductionFactors);
+
+disp('Mean and Standard Deviation of Reconstruction Times:');
+disp(meanReconstructionTimes);
+disp(stdDevReconstructionTimes);
+
+disp('Mean and Standard Deviation of Scan Times:');
+disp(meanScanTimes);
+disp(stdDevScanTimes);
+
+disp('Mean and Standard Deviation of Reconstruction Errors:');
+disp(meanReconstructionErrors);
+disp(stdDevReconstructionErrors);
