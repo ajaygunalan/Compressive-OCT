b54ed8db36987fb5e01fe452db4e9846dc06c8b6
Ajay Gunalan <ajaygunalan1995@gmail.com>
2024-01-15 15:07:30 +0100

    To Do Code for  depth  estimataion from before and after ablation

--- ROS_ServerDepthControl.m
+++ ROS_ServerDepthControl.m
@@ -2,7 +2,7 @@ rosshutdown;  clear all; clc; close all;
 %%
 % Initialize ROS with a specific master IP
 rosinit('10.240.23.49');
-
+ 
 % Define service server
 service = rossvcserver('/estimate_depth', 'oct_msgs/Depth', @serviceCallback);
 
@@ -14,7 +14,17 @@ disp('Service Server running. CTRL+C to exit.');
 % Callback function
 function resp = serviceCallback(~,~,resp)
     % Perform imaging
-    % ... Imaging code goes here ...
+    % Prompt the user for the trial number
+    trialNum = input('Enter the trial number: ', 's');
+    scanNum = input('Enter the scan number: ', 's');
+    commandStr = ['.\\bin\\x64\\Release\\OCTImageCapture.exe ', trialNum, ' ', scanNum];
+    [status, cmdout] = system(commandStr);
+    % Check the status and display the output or error message
+    if status == 0
+        disp(['Command executed successfully: ', cmdout]);
+    else
+        disp(['Error in executing command: ', cmdout]);
+    end
 
     % Estimate depth
     % ... Depth estimation code goes here ...
