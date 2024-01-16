901bd127ed5e3e6baebd1b54a7ff7ce7435d334f
Ajay Gunalan <ajaygunalan1995@gmail.com>
2024-01-16 11:58:42 +0100

    OCT server basic structure added; Need to test it in a loop with CALM client

--- ROS_ServerDepthControl.m
+++ ROS_ServerDepthControl.m
@@ -1,23 +1,32 @@
 rosshutdown;  clear all; clc; close all;
 %%
-% Initialize ROS with a specific master IP
 rosinit('10.240.23.49');
- 
-% Define service server
 service = rossvcserver('/estimate_depth', 'oct_msgs/Depth', @serviceCallback);
-
-% Setup cleanup function to ensure rosshutdown is called when script ends
 cleanupObj = onCleanup(@()rosshutdown);
-
 disp('Service Server running. CTRL+C to exit.');
 
+% Prompt the user
+trialNum = input('Enter the trial number: ', 's');
+scanNum = 1;
+
+commandStr = ['.\\bin\\x64\\Release\\OCTImageCapture.exe ', trialNum, ' ', num2str(scanNum)];
+[status, cmdout] = system(commandStr);
+% Check the status and display the output or error message
+if status == 0
+    disp(['Command executed successfully: ', cmdout]);
+else
+    disp(['Error in executing command: ', cmdout]);
+end
+
+
+baseFolder = 'C:\Ajay_OCT\OCT-Guided-AutoCALM\data\getDepthFromSparse3Doct\';
+folderLocation = fullfile(baseFolder, trialNum);
 % Callback function
 function resp = serviceCallback(~,~,resp)
+    persistent before after scanNum;
     % Perform imaging
-    % Prompt the user for the trial number
-    trialNum = input('Enter the trial number: ', 's');
-    scanNum = input('Enter the scan number: ', 's');
-    commandStr = ['.\\bin\\x64\\Release\\OCTImageCapture.exe ', trialNum, ' ', scanNum];
+    scanNum = scanNum + 1;
+    commandStr = ['.\\bin\\x64\\Release\\OCTImageCapture.exe ', trialNum, ' ', num2str(scanNum)];
     [status, cmdout] = system(commandStr);
     % Check the status and display the output or error message
     if status == 0
@@ -27,8 +36,10 @@ function resp = serviceCallback(~,~,resp)
     end
 
     % Estimate depth
-    % ... Depth estimation code goes here ...
-    estimatedDepth = 5.0; % placeholder, replace with actual depth calculation
+    before = readmatrix(['data\getDepthFromSparse3Doct\1\ScanNum_', num2str(scanNum - 1), '_Estimation.csv']);
+    after = readmatrix(['data\getDepthFromSparse3Doct\1\ScanNum_', num2str(scanNum), '_Estimation.csv']);
+    prefix = sprintf('%s_', scanNum);
+    estimatedDepth = depthEstimation(after, before, prefix, folderLocation);
 
     resp.Depth = rosmessage('std_msgs/Float64');
     resp.Depth.Data = estimatedDepth;
