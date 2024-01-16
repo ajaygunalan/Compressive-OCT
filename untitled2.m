1067de8dac0752b670f5e278f8fd8903c28b9bc0
Ajay Gunalan <ajaygunalan1995@gmail.com>
2024-01-16 14:08:05 +0100

    scanNum 1 and 2 works; small bug in depth difference

--- untitled2.m
+++ untitled2.m
@@ -1,10 +1,15 @@
 clear all; close all; clc;
-%%
-before = readmatrix("data\getDepthFromSparse3Doct\1\ScanNum_1_Estimation.csv");
-after = readmatrix("data\getDepthFromSparse3Doct\1\ScanNum_2_Estimation.csv");
-scanNum = "2";
-trialNum = "1";
-baseFolder = 'C:\Ajay_OCT\OCT-Guided-AutoCALM\data\getDepthFromSparse3Doct\';
-prefix = sprintf('%s_', scanNum);
-folderLocation = fullfile(baseFolder, trialNum);
-depth = depthEstimation(after, before,prefix, folderLocation);
+%% Reconstrcution
+
+% scanNum = "2";
+% trialNum = "1";
+
+%% Depeth Estimation
+% before = readmatrix("data\getDepthFromSparse3Doct\1\ScanNum_1_Estimation.csv");
+% after = readmatrix("data\getDepthFromSparse3Doct\1\ScanNum_2_Estimation.csv");
+% scanNum = "2";
+% trialNum = "1";
+% baseFolder = 'C:\Ajay_OCT\OCT-Guided-AutoCALM\data\getDepthFromSparse3Doct\';
+% prefix = sprintf('%s_', scanNum);
+% folderLocation = fullfile(baseFolder, trialNum);
+% depth = depthEstimation(after, before,prefix, folderLocation);
